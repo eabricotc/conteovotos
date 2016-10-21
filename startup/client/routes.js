491298da4080c342898cc6a44f9e5714b5f7ed9e
Enrique Abricot Cerda <eabricot@ampolletaweb.cl>
2016-10-21 17:34:07 -0300

    estructura

---
+++ startup/client/routes.js
@@ -0,0 +1,5 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { AccountsTemplate } from 'meteor/useraccounts:core';
+
+// Import para cargar los js de los templates que utilizara el router
